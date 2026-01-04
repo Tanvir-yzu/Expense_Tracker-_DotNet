52ca614c7acc2fa52fc3ece78a714a4b912755ee
Tanvir Islam <2020tanvir1971@gmail.com>
2026-01-04 23:44:28 +0800

    chore: update build artifacts and assembly info after changes

--- obj/Debug/net9.0/Expense_Tracker​.AssemblyInfo.cs
+++ obj/Debug/net9.0/Expense_Tracker​.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense_Tracker​")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2435c962d735393d74cd4977d66ae06c23439b59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b04560c143ae46376b52f33580099985ab715c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense_Tracker​")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense_Tracker​")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
